29558789c9843c0aab01b2aac4fe9b26c28cef87
shuhrat71 <toshxujayivshuhrat7@gmail.com>
2024-06-14 15:07:14 +0500

    added refresh function

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ let imgBox = document.getElementById("imgBox");
 let imgQR = document.getElementById("imgQR");
 let textQR = document.getElementById("textQR");
 
+if (localStorage.getItem("qrText")) {
+  textQR.value = localStorage.getItem("qrText");
+}
+
+function saveQRText() {
+  localStorage.setItem("qrText", textQR.value);
+}
+
+textQR.addEventListener("input", saveQRText);
+
 function generateQR() {
   imgQR.src =
     "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
     textQR.value;
 }
+
+generateQR();
+
